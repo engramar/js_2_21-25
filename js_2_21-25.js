3b6f6d1af61b08177e9786e591e838b9095529d1
Engramar Bollas <engramar.bollas@gmail.com>
2020-11-27 22:02:10 +1100

    Create js_2_21-25.js

---
+++ js_2_21-25.js
@@ -0,0 +1,40 @@
+#Use class Syntax to Define a Constructor Function
+// Only change code below this line
+class Vegetable {
+  constructor(name) {
+    this.name = name;
+  }
+}
+// Only change code above this line
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // Should display 'carrot'
+
+#Use getters and setters to Control Access to an Object
+// Only change code below this line
+class Thermostat {
+  constructor(fahrenheit) {
+    this.fahrenheit = fahrenheit;
+  }
+  
+  get temperature() {
+    return (5 / 9) * (this.fahrenheit - 32);
+  }
+  
+  set temperature(celsius) {
+    this.fahrenheit = (celsius * 9.0) / 5 + 32;
+  }
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
+
+#Create a Module Script
+
+
+
+
+#Use export to Share a Code Block
+#Reuse JavaScript Code Using import
